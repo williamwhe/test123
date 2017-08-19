d393688e9e90dd030b1752588b9cd24f07dd8a82
xiaocw <chaoweixiao11@gmail.com>
2017-08-19 02:51:47 -0700

    remove some parameters

--- opts.py
+++ opts.py
@@ -5,7 +5,7 @@ def parse_opt():
     parser = argparse.ArgumentParser()
     # Data input settings
 
-    parser.add_argument('--ld', type=int, default = 0.8,
+    parser.add_argument('--ld', type=int, default = 0.5,
                     help='lambda_ratio')      
     parser.add_argument('--h_dim', type=int, default = 128,
                     help='hidden_dim')       
@@ -46,42 +46,6 @@ def parse_opt():
     parser.add_argument('--decay_iteration_max', type=int, default= 10000, help='decay iteration max')
     parser.add_argument('--learning_rate_decay_every', type=int, default=10, 
                     help='every how many iterations thereafter to drop LR by half?(in epoch)')
-    parser.add_argument('--optim_alpha', type=float, default=0.8,
-                    help='alpha for adam')
-    parser.add_argument('--optim_beta', type=float, default=0.999,
-                    help='beta used for adam')
-    parser.add_argument('--optim_epsilon', type=float, default=1e-8,
-                    help='epsilon that goes into denominator for smoothing')
-
-    #Optimization: for the CNN
-    parser.add_argument('--cnn_optim', type=str, default='adam',
-                    help='optimization to use for CNN')
-    parser.add_argument('--cnn_optim_alpha', type=float, default=0.8,
-                    help='alpha for momentum of CNN')
-    parser.add_argument('--cnn_optim_beta', type=float, default=0.999,
-                    help='beta for momentum of CNN')
-    parser.add_argument('--cnn_learning_rate', type=float, default=1e-5,
-                    help='learning rate for the CNN')
-    parser.add_argument('--cnn_weight_decay', type=float, default=0,
-                    help='L2 weight decay just for the CNN')
-
-    # Evaluation/Checkpointing
-    parser.add_argument('--val_images_use', type=int, default=1000,
-                    help='how many images to use when periodically evaluating the validation loss? (-1 = all)')
-    parser.add_argument('--save_checkpoint_every', type=int, default=2500,
-                    help='how often to save a model checkpoint (in iterations)?')
-    parser.add_argument('--language_eval', type=int, default=0,
-                    help='Evaluate language as well (1 = yes, 0 = no)? BLEU/CIDEr/METEOR/ROUGE_L? requires coco-caption code from Github.')
-    parser.add_argument('--losses_log_every', type=int, default=25,
-                    help='How often do we snapshot losses, for inclusion in the progress dump? (0 = disable)')       
-    parser.add_argument('--load_best_score', type=int, default=1,
-                    help='Do we load previous best score when resuming training.')       
-
-    # misc
-    parser.add_argument('--id', type=str, default='',
-                    help='an id identifying this run/job. used in cross-val and appended when writing progress files')
-    parser.add_argument('--train_only', type=int, default=0,
-                    help='if true then use 80k, else use 110k')
 
     args = parser.parse_args()
 
--- trainer.py
+++ trainer.py
@@ -22,13 +22,13 @@ from evagan import EvaGAN
 def train():
     opt = opts.parse_opt()
 
-    # mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-    mnist = sio.loadmat('MNIST_data/mnist.mat')
-    train_data =  mnist['train_data']
-    train_label = mnist['train_label']
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    # mnist = sio.loadmat('MNIST_data/mnist.mat')
+    train_data =  mnist.train.images * 2.0 - 1.0
+    train_label = mnist.train.labels
     loader = Dataset(train_data, train_label)
-    test_data = mnist['test_data']
-    test_label = mnist['test_label']
+    test_data = mnist.test.images * 2.0 - 1.0
+    test_label = mnist.test.labels
     test_loader = Dataset(test_data, test_label)
 
     x_dim = train_data.shape[1]
@@ -151,7 +151,7 @@ def train():
                 fig = plot(samples)
                 plt.savefig( opt.image_path + '/{}.png'.format(str(iteration)), bbox_inches='tight')
                 ### save log into a file 
-                with open(opt.checkpoint_path + "result.txt", 'a') as fid :
+                with open(opt.checkpoint_path + "/result.txt", 'a') as fid :
                     log = "iteration:{}, attack rate: {} \n".format( str(iteration), str(acc / float(i * batch_size) ))
                     fid.write(log)
                 checkpoint_path = os.path.join(opt.checkpoint_path, 'model.ckpt')
@@ -160,49 +160,43 @@ def train():
 
 def test():
     opt = opts.parse_opt()
-    # mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-    mnist = sio.loadmat('MNIST_data/mnist.mat')
-    train_data =  mnist['train_data']
-    train_label = mnist['train_label']
+
+    mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+    # mnist = sio.loadmat('MNIST_data/mnist.mat')
+    train_data =  mnist.train.images * 2.0 - 1.0
+    train_label = mnist.train.labels
     loader = Dataset(train_data, train_label)
-    test_data = mnist['test_data']
-    test_label = mnist['test_label']
+    test_data = mnist.test.images * 2.0 - 1.0
+    test_label = mnist.test.labels
     test_loader = Dataset(test_data, test_label)
 
     x_dim = train_data.shape[1]
     y_dim = train_label.shape[1]
+    # pdb.set_trace()
     batch_size = opt.batch_size
+    h_dim = opt.h_dim
 
     opt.input_c_dim = 1
     opt.output_c_dim = 1
     opt.input_dim = x_dim
     opt.label_dim = y_dim
-    opt.img_dim = 28
-
-    opts.str_iter = 7500
-    # opts.str_iter = 2000
-   
+    loader = Dataset(train_data, train_label)
+    fine_tune = opt.fine_tune
     D = MLP(layers = (h_dim))
-    # D.train(train_data, train_label)
-    # D.save("save/mlp_model")
-    D.load("save/mlp_model")
-    #pdb.set_trace()
-    # D.predict( train_data[0:10], train_label[0:10])
-    #pdb.set_trace()
+    D.load( opt.checkpoint_path + "/mlp_model")
     NUM_THREADS = 2
     tf_config = tf.ConfigProto()
     tf_config.intra_op_parallelism_threads=NUM_THREADS
     tf_config.gpu_options.allow_growth = True
     with tf.Session(config=tf_config) as sess:
-        # Initialize the variables, and restore the variables form checkpoint if there is.
-        # and initialize the writer
-
-        model = EavGAN(D, opts, sess)
+        
+        model = EavGAN(D, opt, sess)
 #        pdb.set_trace()
-        iteration = opts.str_iter
-        checkpoint_path = os.path.join(opt.checkpoint_path, 'model.ckpt')
+        iteration = opt.iteration
+        checkpoint_path = os.path.join(opt.load_checkpoint_path, 'model.ckpt')
         checkpoint_path += "-" + str(iteration)
         model.load(checkpoint_path)
+        epoch = opt.pretrain_epoch + 1
         num = test_loader._num_examples
         iter_num = (num - batch_size )  / batch_size
         acc = 0.0
